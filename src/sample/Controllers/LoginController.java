b2f0f91cbcf8e3730fcbdc918df2f7e2e123720e
LabUser <zsmit75@wgu.edu>
2022-10-26 16:34:59 -0600

    New

---
+++ src/sample/Controllers/LoginController.java
@@ -0,0 +1,8 @@
+package sample.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+}
---
+++ src/sample/Models/Users.java
@@ -0,0 +1,2 @@
+package sample.Models;public class Users {
+}
